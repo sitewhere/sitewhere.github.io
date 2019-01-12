1ddca134bc430eb59abfaf8472ce5b0e02d14ec6
Travis CI User <travis@example.org>
2019-01-12 22:59:30 +0000

    Automated documentation update.

--- docs/2.0.0/assets/js/7.13275b44.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{238:function(t,n,e){"use strict";e.r(n);var s=e(1),i=Object(s.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"content"},[n("Seo")],1)},[],!1,null,null,null);i.options.__file="README.md";n.default=i.exports}}]);
